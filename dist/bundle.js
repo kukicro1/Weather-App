a5da20ef654b08fcf8d71dad987e8be69abfdfb8
kukicro1 <krunomraz@gmail.com>
2023-02-02 19:21:28 +0100

    Trying to fix ESLint by deleting node_modules folder from gitignore

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-
-/** *** */ (() => {
-  // webpackBootstrap
-  const __webpack_exports__ = {};
-  /*! *************************!*\
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*************************!*\
   !*** ./src/js/index.js ***!
-  \************************ */
+  \*************************/
+console.log("krunoooooo");
+let kruno = 2;
+kruno = "prva cura";
+console.log(kruno);
+console.log("aaaaaaa");
 
-  /** *** */
-})();
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYXRoZXJfYXBwLy4vc3JjL2pzL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwia3J1bm9vb29vb1wiKTtcbmxldCBrcnVubyA9IDI7XG5rcnVubyA9IFwicHJ2YSBjdXJhXCI7XG5jb25zb2xlLmxvZyhrcnVubyk7XG5jb25zb2xlLmxvZyhcImFhYWFhYWFcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+console.log("krunoooooo");
+let kruno = 2;
+kruno = "prva cura";
+console.log(kruno);
+console.log("aaaaaaa");
